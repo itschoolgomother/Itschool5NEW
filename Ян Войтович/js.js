3cb5fbca8d4d44de05cca71902324cfce70483b5
itschoolgomother <itschoolgomother@gmail.com>
2020-10-12 19:00:06 +0300

    12.10.2020

---
+++ Ян Войтович/js.js
@@ -0,0 +1,13 @@
+function meetUp(N, C) {
+	let meets = (12-C)/N;
+  return Math.floor(meets);
+
+}
+let num = 5;
+console.log("num");
+console.log('num');
+
+function getExchange(vidVolut, kursVolut, summa){
+	let kolovo = kursVolut*summa;
+return `мы вам дадим ${kolovo} ${vidVolut}`;
+}
