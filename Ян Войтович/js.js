f0f18b3770f87d402f69f21ad5875d0aa19b8f8f
itschoolgomother <itschoolgomother@gmail.com>
2020-11-08 13:33:57 +0200

    8.11.2020

--- Ян Войтович/js.js
+++ Ян Войтович/js.js
@@ -39,15 +39,27 @@ function sravnenie(){
 // for (let i = 0; i <= 12; i++){
 // 	console.log(sts[i]);
 // }
-function shark(sharkDistanse=100,yourDistanse=6,sharkSpeed=50000,yourSpeed=4,dolphin=3) {
-	let dobleSpeed = yourSpeed*2;
-	if(dolphin){
-		yourSpeed = dobleSpeed;
-	}
-	if(yourDistanse/yourSpeed < sharkDistanse/sharkSpeed){
-	return "alive";
-	}else{
-	return "dead";
-}
-}
+// function shark(sharkDistanse=100,yourDistanse=6,sharkSpeed=50000,yourSpeed=4,dolphin=3) {
+// 	let dobleSpeed = yourSpeed*2;
+// 	if(dolphin){
+// 		yourSpeed = dobleSpeed;
+// 	}
+// 	if(yourDistanse/yourSpeed < sharkDistanse/sharkSpeed){
+// 	return "alive";
+// 	}else{
+// 	return "dead";
+// }
+// }
+document.write("пипипуйка")
 
+let str = "s1ba1v1";
+function answer(){
+let odin = 0;
+
+for (let i = 0; i < str.length; i ++){
+	if(1 == str[i]){
+		odin = odin + 1;
+	}
+}
+document.write(odin);
+}
