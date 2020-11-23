7655955d6738196b9bc0cbf301528781454747d9
itschoolgomother <itschoolgomother@gmail.com>
2020-11-23 20:27:03 +0200

    23.11.2020

---
+++ Ян Войтович/js2.js
@@ -0,0 +1,9 @@
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+x
+
+ctx.beginPath();
+ctx.rect(50,100,300,200);
+ctx.closePath();
+ctx.fillStyle = "teal";
+ctx.fill;
